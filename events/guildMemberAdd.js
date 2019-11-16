94869e5e5a10aa0d897b1a20102bb21983c814cf
SimplySublime <koryfreese@gmail.com>
2019-11-16 04:05:26 -0800

    Move events into dedicated folder

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,15 @@
+const client = require('../index')
+
+client.on('guildMemberAdd', (member) => {
+  const { prefix } = client.config.general
+  const embed = new RichEmbed()
+  embed.setColor(3447003)
+  embed.setThumbnail(member.guild.iconURL)
+  embed.setAuthor(member.user.username, member.user.avatarURL)
+  embed.setTitle(`Welcome To ${member.guild.name}!`)
+  embed.setDescription(
+    `Please take a look at our rules by typing **${prefix}rules**!\nView our commands with **${prefix}help**\nEnjoy your stay!`
+  )
+  const channel = member.guild.channels.get(client.config.general.welcomeChannel)
+  return channel.send({ embed })
+})
