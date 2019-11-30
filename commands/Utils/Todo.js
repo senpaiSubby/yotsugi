ec058a3b9e1c26a225dd5c967a9df2ff4a155d09
SimplySublime <koryfreese@gmail.com>
2019-11-30 05:42:09 -0800

    Can control todo list via reactions

--- commands/Utils/Todo.js
+++ commands/Utils/Todo.js
@@ -12,7 +12,7 @@ module.exports = class Todo extends Command {
 
   async run(client, msg, args) {
     const { p, Utils, memberConfig } = client
-    const { warningMessage, standardMessage, embed } = Utils
+    const { standardMessage, embed, asyncForEach, warningMessage } = Utils
     const { author } = msg
 
     const todo = args.slice(1).join(' ')
@@ -23,40 +23,89 @@ module.exports = class Todo extends Command {
 
     switch (args[0]) {
       case 'add': {
+        if (todos.length >= 10) {
+          return warningMessage(msg, 'You cannot have more than [ 10 ] todos!')
+        }
+
+        if (!todo) {
+          const m = await warningMessage(msg, 'Todo cannot be empty!')
+          return m.delete(3000)
+        }
+
         todos.push(todo)
         await db.update({ config: JSON.stringify(config) })
-
-        return standardMessage(msg, `[ ${todo} ]\n\nAdded to todo list`)
-      }
-
-      case 'remove': {
-        const item = args[1] - 1
-        const name = todos[item]
-        todos.splice(item, 1)
-        await db.update({ config: JSON.stringify(config) })
-
-        if (name) return standardMessage(msg, `[ ${name} ]\n\nRemoved from todo list`)
-
-        return warningMessage(msg, `Todo [ ${name} ] does not exist`)
+        return standardMessage(msg, `Added [ ${todo} ] to todo list`)
       }
 
       default: {
         if (!todos.length) {
           return msg.reply(
             embed(msg, 'yellow')
-              .setTitle(`There are no todos!`)
-              .setDescription(`\`${p}todos add <todo to add>\`\nTo add some!`)
+              .setTitle(`Todo list is empty!`)
+              .setDescription(`\`${p}todos add <todo to add>\` to add one`)
           )
         }
 
-        let todoList = ''
-        todos.forEach((i, index) => (todoList += `${index + 1} | ${i}\n`))
+        const reactions = [
+          '\u0031\u20E3',
+          '\u0032\u20E3',
+          '\u0033\u20E3',
+          '\u0034\u20E3',
+          '\u0035\u20E3',
+          '\u0036\u20E3',
+          '\u0037\u20E3',
+          '\u0038\u20E3',
+          '\u0039\u20E3',
+          '🔟'
+        ]
+        // setup inital embed
+        const e = embed(msg).setTitle('Todo List')
+        todos.forEach((i, index) => e.addField(`[ ${index + 1} ]`, `${i}`, true))
+        const m = await msg.reply(e)
 
-        return msg.reply(
-          embed(msg)
-            .setTitle('Todos')
-            .setDescription(todoList)
-        )
+        await asyncForEach(todos, async (i, index) => {
+          await m.react(reactions[index])
+        })
+        await m.react('🛑')
+
+        // setup reaction collector
+        const filter = (reaction, user) => {
+          const { name } = reaction.emoji
+          if (reactions.includes(name) && user.id === author.id) return true
+          if (name === '🛑' && user.id === author.id) return true
+        }
+        const collector = m.createReactionCollector(filter, { max: 11, time: 3600000 })
+
+        collector.on('collect', async (a) => {
+          if (a.emoji.name === '🛑') return m.clearReactions()
+
+          // find index
+          const index = reactions.findIndex((i) => i === a.emoji.name)
+
+          // remove and notify todo is removed
+          const name = todos[index]
+          todos.splice(index, 1)
+          await db.update({ config: JSON.stringify(config) })
+          const removeMessage = await standardMessage(msg, `[ ${name} ] removed from todo list`)
+          removeMessage.delete(2000)
+
+          // edit original embed with updated content
+          await m.clearReactions()
+          if (!todos.length) {
+            return m.edit(
+              embed(msg, 'yellow')
+                .setTitle(`There are no more todos!`)
+                .setDescription(`\`${p}todos add <todo to add>\` to add one`)
+            )
+          }
+          const newEmbed = embed(msg).setTitle('Todo List')
+          todos.forEach((i, ind) => newEmbed.addField(`[ ${ind + 1} ]`, `${i}`, true))
+          await m.edit(newEmbed)
+          await asyncForEach(todos, async (i, ind) => {
+            await m.react(reactions[ind])
+          })
+          await m.react('🛑')
+        })
       }
     }
   }
