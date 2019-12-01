cc626539f2370481fbb374b65f9117776416bb57
SimplySublime <koryfreese@gmail.com>
2019-11-30 18:19:04 -0800

    Move config checking to  startup instead of on message

--- index.js
+++ index.js
@@ -30,6 +30,43 @@ client.colors = {
 
 module.exports = { client }
 
+// handle DB config
+const handleConfig = async () => {
+  const config = await client.generalConfig.findOne({ where: { id: client.config.ownerID } })
+
+  if (!config) {
+    client.Log.info('Handle Config', `Created new general config for [ ${client.config.ownerID} ]`)
+    await client.generalConfig.create({
+      username: 'Nezuko',
+      id: client.config.ownerID,
+      config: JSON.stringify({
+        archivebox: { path: null },
+        autocmd: [],
+        disabledCommands: [],
+        docker: { host: null },
+        emby: { apiKey: null, host: null, userID: null },
+        googleHome: { ip: null, language: null, name: null },
+        google: { apiKey: null },
+        jackett: { apiKey: null, host: null },
+        meraki: { apiKey: null, serielNum: null },
+        ombi: { apiKey: null, host: null, username: null },
+        pihole: { apiKey: null, host: null },
+        pioneerAVR: { host: null },
+        rclone: { remote: null },
+        routines: [],
+        sabnzbd: { apiKey: null, host: null },
+        sengled: { jsessionid: null, password: null, username: null },
+        shortcuts: [],
+        systemPowerControl: [{ host: 'xxx', mac: 'xxx', name: 'xxx' }],
+        transmission: { host: null, port: '9091', ssl: false },
+        tuyaDevices: [{ id: 'xxxxxxx', key: 'xxx', name: 'xxx' }],
+        webUI: { apiKey: '111', commands: [] }
+      })
+    })
+  }
+}
+handleConfig()
+
 // Load event handlers
 const eventFiles = client.Utils.findNested('./events', '.js')
 eventFiles.forEach((file) => require(file))
