c51c5a900950d2d3c2e6b89d3f2387f0934b55cf
SimplySublime <koryfreese@gmail.com>
2020-01-05 06:31:35 -0800

    Remove build folder

--- build/core/database/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/*!
- * Coded by CallMeKory - https://github.com/callmekory
- * 'It’s not a bug – it’s an undocumented feature.'
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("./database");
-exports.default = database_1.database;
--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*!
- * Coded by CallMeKory - https://github.com/callmekory
- * 'It’s not a bug – it’s an undocumented feature.'
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const NezukoClient_1 = require("./NezukoClient");
-// Start Nezuko
-new NezukoClient_1.NezukoClient().start();
