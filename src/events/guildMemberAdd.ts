97c1e51b827422a0411b435c1b5fc9d06c493d64
koru <koryfreese@gmail.com>
2020-01-06 22:32:20 -0800

    Add in join/leave images

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -3,25 +3,71 @@
  * 'It’s not a bug – it’s an undocumented feature.'
  */
 
+import Canvas from 'canvas'
 import { GuildMember, TextChannel } from 'discord.js'
+// tslint:disable-next-line:no-duplicate-imports
+import Discord from 'discord.js'
+import path from 'path'
 import { database } from '../core/database/database'
-import { Utils } from '../core/utils/Utils'
 
 export const guildMemberAdd = async (member: GuildMember) => {
-  const { embed } = Utils
-
   const db = await database.models.ServerConfig.findOne({ where: { id: member.guild.id } })
   const { welcomeChannel, prefix } = JSON.parse(db.get('config') as string)
 
-  const e = embed()
-    .setColor('RANDOM')
-    .setThumbnail(member.guild.iconURL)
-    .setAuthor(member.user.username, member.user.avatarURL)
-    .setTitle(`Welcome To ${member.guild.name}!`)
-    .setDescription(
-      `Please take a look at our rules by typing **${prefix}rules**!\nView our commands with **${prefix}help**\nEnjoy your stay!`
-    )
-
   const channel = member.guild.channels.get(welcomeChannel) as TextChannel
-  return channel.send(e)
+
+  /////////////////////////////////////////////////////////////////////
+  if (!channel) return
+
+  Canvas.registerFont(path.join(__dirname, '..', 'core', 'images', 'GENUINE.ttf'), {
+    family: 'GENUINE'
+  })
+
+  const canvas = Canvas.createCanvas(800, 400)
+
+  const ctx = canvas.getContext('2d')
+
+  const background = await Canvas.loadImage(`${__dirname}/../core/images/welcome.png`)
+  ctx.drawImage(background, 0, 0, canvas.width + 2, canvas.height + 2)
+
+  ctx.strokeStyle = '#74037b'
+  ctx.strokeRect(0, 0, canvas.width, canvas.height)
+
+  // Greetings
+  ctx.font = '50px GENUINE'
+  ctx.fillStyle = '#ffffff'
+  ctx.fillText(`GREETINGS:`, 20, 380)
+
+  // Username
+  ctx.font = '50px GENUINE'
+  ctx.fillStyle = '#a2ba00'
+  const textLength = member.user.username.length
+  ctx.fillText(member.user.username.toUpperCase(), 280 - textLength, 380)
+
+  // Rules
+  ctx.font = '30px GENUINE'
+  ctx.fillStyle = '#ffffff'
+  ctx.fillText(`View our rules with [ ${prefix}rules ]`, 420, 40)
+
+  // Roles
+  ctx.font = '30px GENUINE'
+  ctx.fillStyle = '#ffffff'
+  ctx.fillText(`View our roles with [ ${prefix}roles ]`, 420, 70)
+
+  // Pick up the pen
+  ctx.beginPath()
+  // Start the arc to form a circle
+  ctx.arc(120, 120, 100, 0, Math.PI * 2)
+  // Put the pen down
+  ctx.closePath()
+  // Clip off the region you drew on
+  ctx.clip()
+
+  // Avatar
+  const avatar = await Canvas.loadImage(member.user.displayAvatarURL)
+  ctx.drawImage(avatar, 20, 20, 200, 200)
+
+  const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png')
+
+  channel.send(`Welcome home, ${member}!`, attachment)
 }
--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -3,23 +3,61 @@
  * 'It’s not a bug – it’s an undocumented feature.'
  */
 
+import Canvas from 'canvas'
 import { GuildMember, TextChannel } from 'discord.js'
+// tslint:disable-next-line:no-duplicate-imports
+import Discord from 'discord.js'
+import path from 'path'
 import { database } from '../core/database/database'
-import { Utils } from '../core/utils/Utils'
 
 export const guildMemberRemove = async (member: GuildMember) => {
-  const { embed } = Utils
-
   const db = await database.models.ServerConfig.findOne({ where: { id: member.guild.id } })
   const { welcomeChannel } = JSON.parse(db.get('config') as string)
 
-  const e = embed()
-    .setColor('RANDOM')
-    .setThumbnail(member.guild.iconURL)
-    .setAuthor(member.user.username, member.user.avatarURL)
-    .setTitle(`Left the server!`)
-    .setDescription(`Sorry to see you go!`)
-
   const channel = member.guild.channels.get(welcomeChannel) as TextChannel
-  return channel.send(e)
+
+  /////////////////////////////////////////////////////////////////////
+  if (!channel) return
+
+  Canvas.registerFont(path.join(__dirname, '..', 'core', 'images', 'GENUINE.ttf'), {
+    family: 'GENUINE'
+  })
+
+  const canvas = Canvas.createCanvas(800, 400)
+
+  const ctx = canvas.getContext('2d')
+
+  const background = await Canvas.loadImage(`${__dirname}/../core/images/welcome.png`)
+  ctx.drawImage(background, 0, 0, canvas.width + 2, canvas.height + 2)
+
+  ctx.strokeStyle = '#74037b'
+  ctx.strokeRect(0, 0, canvas.width, canvas.height)
+
+  // Greetings
+  ctx.font = '50px GENUINE'
+  ctx.fillStyle = '#ffffff'
+  ctx.fillText(`GOODBYE!:`, 20, 380)
+
+  // Username
+  ctx.font = '50px GENUINE'
+  ctx.fillStyle = '#a2ba00'
+  const textLength = member.user.username.length
+  ctx.fillText(member.user.username.toUpperCase(), 280 - textLength, 380)
+
+  // Pick up the pen
+  ctx.beginPath()
+  // Start the arc to form a circle
+  ctx.arc(120, 120, 100, 0, Math.PI * 2)
+  // Put the pen down
+  ctx.closePath()
+  // Clip off the region you drew on
+  ctx.clip()
+
+  // Avatar
+  const avatar = await Canvas.loadImage(member.user.displayAvatarURL)
+  ctx.drawImage(avatar, 20, 20, 200, 200)
+
+  const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png')
+
+  channel.send(`${member.user.tag} has left the server`, attachment)
 }
