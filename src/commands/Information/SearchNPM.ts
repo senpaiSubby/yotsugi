1257e4800b13006dfa2c7054127ea7ea80cc2a7a
callmekory <koryfreese@gmail.com>
2020-03-10 20:39:30 -0700

    Added NPM search command

---
+++ src/commands/Information/SearchNPM.ts
@@ -0,0 +1,57 @@
+/*!
+ * Coded by CallMeKory - https://github.com/callmekory
+ * 'It’s not a bug – it’s an undocumented feature.'
+ */
+import { NezukoMessage } from 'typings'
+import { BotClient } from '../../core/BotClient'
+
+import { Command } from '../../core/base/Command'
+
+export default class SearchNPM extends Command {
+  constructor(client: BotClient) {
+    super(client, {
+      name: 'npm',
+      category: 'Information',
+      description: 'Search the NPM package repos',
+      usage: ['npm [search term]'],
+      args: true
+    })
+  }
+
+  public async run(client: BotClient, msg: NezukoMessage, args: any[]) {
+    const { execAsync, warningMessage, embed, paginate } = client.Utils
+
+    // Get search term from user input
+    const searchTerm = args.join(' ')
+
+    const { code, stdout: results } = await execAsync(
+      `npm search --json ${searchTerm}`
+    )
+
+    if (results && code === 0) {
+      const embedList = JSON.parse(results).map((r) => {
+        const { name, version, description, date, links, author } = r
+
+        return embed(msg, 'red')
+          .setTitle(`NPM Search [ ${name} ]`)
+          .setThumbnail(
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/540px-Npm-logo.svg.png'
+          )
+          .setDescription(description)
+          .setURL(links.npm)
+          .addField('Author', author.username, true)
+          .addField('Version', version, true)
+          .addField('Last Updated', new Date(date).toDateString())
+      })
+
+      return paginate(msg, embedList)
+    }
+
+    if (code !== 0) return warningMessage(msg, 'An error occurred with NPM')
+
+    return warningMessage(
+      msg,
+      `Couldn't find any NPM packages matching [ ${searchTerm} ]`
+    )
+  }
+}
